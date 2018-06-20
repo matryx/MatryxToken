a12aed6da2a03b2113671c5f4a0178c0c232a327
shessenauer <sam.hessenauer@gmail.com>
2018-06-19 17:39:28 -0700

    updated for a quick and easy migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,21 +3,12 @@ var Crowdsale = artifacts.require('./Crowdsale.sol')
 
 var test = true;
 
-// test constructor args manually
-const _presaleStartTime = 1506399909
-const _startTime = 1508991909
-const _endTime = 1511673909
-const _wallet = "0x01da6F5F5C89F3a83CC6BeBb0eAFC1f1E1c4A303"
+// // test constructor args manually
+// const _presaleStartTime = 1506399909
+// const _startTime = 1508991909
+// const _endTime = 1511673909
+// const _wallet = "0x01da6F5F5C89F3a83CC6BeBb0eAFC1f1E1c4A303"
 
 module.exports = function(deployer) {
-  if(test) {
-  	MatryxToken.new().then(function(res) {
-  	  deployer.deploy(Crowdsale, _presaleStartTime, _startTime, _endTime, _wallet, res.address)
-  	})
-  } else {
-    //deployer.deploy(Crowdsale, 1504052905, 1504056025, 1504059625, "0x0040077926585455c40ceA126B37bED392aCa8C2")
-	MatryxToken.new().then(function(res) {
-	  deployer.deploy(Crowdsale, 1504062025, 1504062925, 1504063825, "0x0040077926585455c40ceA126B37bED392aCa8C2", res.address)
-	})
-  }
+deployer.deploy(MatryxToken)
 };
--- truffle.js
+++ truffle.js
@@ -5,7 +5,7 @@ var test = true
 var account
 
 if(test){
-  account = "0x01da6f5f5c89f3a83cc6bebb0eafc1f1e1c4a303"
+  account = "0x606849bb8161f2d6de55c7d305af3f6408cacf2a"
 } else {
   account = "0x0040077926585455c40ceA126B37bED392aCa8C2"
 }
